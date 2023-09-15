184ac254807851015dd779ab88ac297a869cd09a
mvieira2020 <mvieira2020@fau.edu>
2023-09-15 15:57:51 -0400

    Step 8: Setup custom collection view cell

---
+++ lab-tunley/AlbumViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  AlbumViewCell.swift
+//  lab-tunley
+//
+//  Created by Michelle Vieira on 9/15/23.
+//
+
+import UIKit
+
+class AlbumViewCell: UICollectionViewCell {
+    @IBOutlet weak var albumImageView: UIImageView!
+    
+}
